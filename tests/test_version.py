2bda6174a901caaeb30046ab5ebac9d78308ce9a
dmontaner <david.montaner@gmail.com>
2020-05-31 15:53:57 +0100

    cleaning code

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,11 +1,10 @@
-import pandas as pd
 import pandas_save_profiler
+
+# import pandas as pd
+# def test_pandas_version():
+#     assert pd.__version__ == '1.0.3'
 
 
 def test_version():
     """Test that the version exists"""
     assert '__version__' in dir(pandas_save_profiler)
-
-
-# def test_pandas_version():
-#     assert pd.__version__ == '1.0.3'
